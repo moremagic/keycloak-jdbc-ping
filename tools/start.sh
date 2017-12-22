145e6cb661c0cc7056040b2d92c4d04f2fda17f3
moremagic <itoumagic@gmail.com>
2017-12-22 18:06:41 +0900

    JGroups リッスンポートの指定方法を変更 起動シェルの最適化

--- tools/start.sh
+++ tools/start.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/opt/jboss/docker-entrypoint.sh -c standalone-ha.xml -b 0.0.0.0 -Djboss.bind.address.private=$(getAddress.py) -Djboss.bind.address.management=$(getAddress.py) -Djboss.bind.address=$(getAddress.py)
+/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$(getAddress.py) -Djboss.bind.address.management=$(getAddress.py) -Djgroups.bind.address=$(getAddress.py)
