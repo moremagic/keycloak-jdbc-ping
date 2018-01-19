ef805834f18828de634cb5feb3597d0d1da59657
moremagic <itoumagic@gmail.com>
2018-01-19 13:38:16 +0900

    ECSで host名からIPアドレスが引けないため /etc/hosts へホスト名登録出来るように対応

--- tools/start.sh
+++ tools/start.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}') | cut -d" " -f 7 | cut -d/ -f 1`
+if [ "$(hostname -i)" = "" ]; then
+    echo $HOST_ADDR    $(hostname) >> /etc/hosts
+fi
 /opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR
 
