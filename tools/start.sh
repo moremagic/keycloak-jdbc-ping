4e67e5f20cfb0292aa4a9d2641406ff4edd9970d
moremagic <itoumagic@gmail.com>
2018-01-19 14:07:05 +0900

    DEBUG のためホスト名から取得したIPアドレスを起動時に表示する

--- tools/start.sh
+++ tools/start.sh
@@ -3,5 +3,7 @@ HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}'
 if [ "$(hostname -i)" = "" ]; then
     echo $HOST_ADDR    $(hostname) >> /etc/hosts
 fi
+
+echo $(hostname -i)
 /opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR
 
