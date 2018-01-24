21e538e5df87e1c2d99db1bab7dbb6a49b202d9b
moremagic <itoumagic@gmail.com>
2018-01-24 16:17:26 +0900

    調査のためSSHを追加

--- tools/start.sh
+++ tools/start.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
-HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}') | cut -d" " -f 7 | cut -d/ -f 1`
+
+/usr/sbin/sshd -D &
+
 
 echo $(hostname -i)
 cat /etc/hosts
 
+HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}') | cut -d" " -f 7 | cut -d/ -f 1`
 /opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$HOST_ADDR 
 
