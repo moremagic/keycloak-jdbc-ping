83b7251203155a54a0d033f266cbc5108793b3a4
moremagic <itoumagic@gmail.com>
2018-01-25 08:37:51 +0900

    NIC setting add.

--- tools/start.sh
+++ tools/start.sh
@@ -2,12 +2,14 @@
 
 /usr/sbin/sshd -D &
 
-echo $(hostname -i)
 
-HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}') | cut -d" " -f 7 | cut -d/ -f 1`
+DEFAULT_NIC=`ip route | grep default | awk '{print $NF}'`
+HOST_ADDR=`ip -f inet -o addr show $DEFAULT_NIC | cut -d" " -f 7 | cut -d/ -f 1`
 echo $HOST_ADDR  $(hostname) >> /etc/hosts
 
+echo $(hostname -i)
+echo DEFAULT_NIC=$DEFAULT_NIC 
 cat /etc/hosts
 
-/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$HOST_ADDR 
+/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$HOST_ADDR -Djgroups.bind_interface_str=$DEFAULT_NIC
 
