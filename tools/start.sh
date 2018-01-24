9b42f6b6b59ffb1905a2d6060a2f08f1997f2e67
moremagic <itoumagic@gmail.com>
2018-01-24 15:29:38 +0900

    host設定の見直し

--- tools/start.sh
+++ tools/start.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}') | cut -d" " -f 7 | cut -d/ -f 1`
-if [ "$(hostname -i)" = "" ]; then
-    sed "/^.*`hostname`$/d" /etc/hosts > ~/hosts.new
-    echo $HOST_ADDR    $(hostname) >> ~/hosts.new
-    cp /etc/hosts /etc/hosts.org && cp ~/hosts.new /etc/hosts
-fi
 
 echo $(hostname -i)
 cat /etc/hosts
-/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr= 
+
+/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$HOST_ADDR 
 
