51e6c837cf0d232a0d20a12a5b2cec76a4df17d1
moremagic <itoumagic@gmail.com>
2018-01-24 18:25:34 +0900

    hosts fix.

--- tools/start.sh
+++ tools/start.sh
@@ -2,10 +2,12 @@
 
 /usr/sbin/sshd -D &
 
-
 echo $(hostname -i)
-cat /etc/hosts
 
 HOST_ADDR=`ip -f inet -o addr show $(ip route | grep default | awk '{print $NF}') | cut -d" " -f 7 | cut -d/ -f 1`
+echo $HOST_ADDR  $(hostname) >> /etc/hosts
+
+cat /etc/hosts
+
 /opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$HOST_ADDR 
 
