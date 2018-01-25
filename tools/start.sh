f4d872fc38af5e123a495aed5b7be34494532ab0
moremagic <itoumagic@gmail.com>
2018-01-25 09:09:07 +0900

    echo順序の変更

--- tools/start.sh
+++ tools/start.sh
@@ -2,14 +2,13 @@
 
 /usr/sbin/sshd -D &
 
-
 DEFAULT_NIC=`ip route | grep default | awk '{print $NF}'`
 HOST_ADDR=`ip -f inet -o addr show $DEFAULT_NIC | cut -d" " -f 7 | cut -d/ -f 1`
 echo $HOST_ADDR  $(hostname) >> /etc/hosts
+cat /etc/hosts
 
 echo $(hostname -i)
 echo DEFAULT_NIC=$DEFAULT_NIC 
-cat /etc/hosts
 
 /opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=$HOST_ADDR -Djgroups.bind_interface_str=$DEFAULT_NIC
 
