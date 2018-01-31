64f6e763d1ec6416c7c76cf1885de877735fb0cf
moremagic <itoumagic@gmail.com>
2018-01-31 10:23:04 +0900

    起動引数の見直し

--- tools/start.sh
+++ tools/start.sh
@@ -10,5 +10,5 @@ HOST_ADDR=`ip -f inet -o addr show $DEFAULT_NIC | cut -d" " -f 7 | cut -d/ -f 1`
 echo $(hostname -i)
 echo DEFAULT_NIC=$DEFAULT_NIC 
 
-/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true -Djgroups.bind_interface_str=$DEFAULT_NIC -Dignore.bind.address=true
+/opt/jboss/docker-entrypoint.sh $@ -Djboss.bind.address.private=$HOST_ADDR -Djboss.bind.address.management=$HOST_ADDR -Djgroups.bind.address=$HOST_ADDR -Djava.net.preferIPv4Stack=true
 
